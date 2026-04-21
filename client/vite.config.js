90cbb4ae5f8c78f99ded9c3642b1ddde24994553
Travis Fahey <travis_fahey@rocketmail.com>
2026-04-20 21:38:09 -0400

    config fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SDEV_255_FINAL_PROJECT_GROUP2/',
+  base: '/SDEV_255_FINAL_PROJECT_GROUP2/client/',
 })
